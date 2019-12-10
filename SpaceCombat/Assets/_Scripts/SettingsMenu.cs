25e37f5be0baaa368bc1c09658d7749560f84b4c
Morgan Reilly <morgan.reilly27@gmail.com>
2019-12-10 02:41:25 +0000

    Audio Controls
    Added an audio slider to the settings page
    Also made an attempt to implement it into each scene, and it works, but it's not great. (Brain is melted)

---
+++ SpaceCombat/Assets/_Scripts/SettingsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+/** Adapted from: https://youtu.be/YOaYQrN1oYQ */
+
+public class SettingsMenu : MonoBehaviour
+{
+    // Need reference to volume mixer
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("volume", volume);
+    }
+}
